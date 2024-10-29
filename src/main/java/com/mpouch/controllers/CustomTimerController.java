f73d892b0ade2658fa51f0818e4471e8e4896f52
mpouch <177807460+mpouch@users.noreply.github.com>
2024-10-29 12:37:46 -0300

    Design custom timer UI

---
+++ src/main/java/com/mpouch/controllers/CustomTimerController.java
@@ -0,0 +1,26 @@
+package com.mpouch.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CustomTimerController {
+    
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private TextField inpMin;
+
+    @FXML
+    private TextField inpSec;
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    private AnchorPane parent;
+    
+}
