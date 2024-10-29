f92c185d5857ef00ba1f3049a330fc7379d423fa
mpouch <177807460+mpouch@users.noreply.github.com>
2024-10-29 18:13:22 -0300

    Change main frame title

--- src/main/java/com/mpouch/Main.java
+++ src/main/java/com/mpouch/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Java Timer App");
+        stage.setTitle("TimerFX");
 
         stage.setScene(scene);
         stage.show();
